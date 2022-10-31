04aa213737d4cb7c595d47a5035dbf1e3e6ec330
LeonardoVechieti <92764500+LeonardoVechieti@users.noreply.github.com>
2022-10-31 09:42:24 -0300

    Feat: Databese

--- app/src/main/java/com/example/appmusicas/MainActivity.kt
+++ app/src/main/java/com/example/appmusicas/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Room
 import com.example.appmusicas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
@@ -34,6 +35,11 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         super.onResume()
         val recycler = binding.recyclerMusics
 
+        val db = Room.databaseBuilder(
+            this,
+            MusicaDatabase::class.java,
+            "Musica.db"
+        ).allowMainThreadQueries().build()
 
         recycler.adapter = MusicasAdapter(album.getListMusicas(),this)
         recycler.layoutManager = LinearLayoutManager(this)
